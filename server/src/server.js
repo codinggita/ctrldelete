ea78fd212eb301fc5517f52fded1239d6357d5b3
Deepak Kumar <kumar.deepak.cg.s@gmail.com>
2026-07-10 14:11:22 +0530

    feat: setup mongodb connection

--- server/src/server.js
+++ server/src/server.js
@@ -1,9 +1,11 @@
 require('dotenv').config({ path: '../.env' });
 const app = require('./app');
+const connectDB = require('./config/db');
+
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 
-// Start the server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
